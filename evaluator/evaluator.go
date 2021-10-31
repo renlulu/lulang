af5cc15b53de285726ad027a30377cb2a4e440dd
xiaohuo <xiaohuo200@gmail.com>
2021-10-31 18:09:54 +0800

    refactor

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -12,35 +12,6 @@ var (
 	NULL  = &object.Null{}
 )
 
-var builtins = map[string]*object.Builtin{
-	"len": {
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 1 {
-				return newError("wrong number of arguments. got=%d, want=1",
-					len(args))
-			}
-			switch arg := args[0].(type) {
-			case *object.String:
-				return &object.Integer{Value: int64(len(arg.Value))}
-			case *object.Array:
-				return &object.Integer{Value: int64(len(arg.Elements))}
-			default:
-				return newError("argument to `len` not supported, got %s",
-					args[0].Type())
-			}
-		},
-	},
-
-	"print": {
-		Fn: func(args ...object.Object) object.Object {
-			for _, arg := range args {
-				fmt.Println(arg.Inspect())
-			}
-			return NULL
-		},
-	},
-}
-
 func Eval(node ast.Node, env *object.Environment) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
